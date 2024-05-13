af762ec50f10a437dd29328bfaed735d9fa0accb
Kacper Dygoń <dygonkacper@gmail.com>
2024-05-13 23:26:19 +0200

    idk

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 const slideShowItems  = document.querySelectorAll(".slideShowItem");
 const body = document.querySelector("body");
 const carousel = document.querySelector(".slideShow");
+const navUl = document.querySelector("nav ul");
+const nav = document.querySelector("nav");
+const navHamburger = document.querySelector("nav img");
 
+let isNavOpen = false;
+navHamburger.addEventListener("click", alterNav)
+
+navUl.style.left = "-200%";
 
 
     let currentSlideShowIndex = 0;
@@ -14,6 +21,17 @@ function navigateCarousel() {
         currentSlideShowIndex = 0;
     } else {
         carousel.scrollBy(window.innerWidth, 0);
+    }
+    
+}
+
+function alterNav(){
+    if (isNavOpen){
+        navUl.style.left = "-200%";
+        isNavOpen = false;
+    } else {
+        navUl.style.left = "5px";
+        isNavOpen = true;
     }
     
 }
